6494dbe8af4cd48cf229183f1826017bb7f85e84
jdkanuch <jdkanuch@Aeropagadica>
2017-12-28 13:44:55 -0700

    Working binary to codon puzzler system with graphic symbols. Cleaned up main screen and moved to a sub-actor.

--- src/com/genepuzzler/game/screens/MainScreen.java
+++ src/com/genepuzzler/game/screens/MainScreen.java
@@ -2,217 +2,40 @@ package com.genepuzzler.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.genepuzzler.game.GenePuzzler;
+import com.genepuzzler.game.systems.PuzzleGameSystem;
 
 public class MainScreen implements Screen {
 	private GenePuzzler parent;
 	private Stage stage;
-	private Skin skin;
-	private Group cellBoxesGroup;
-	private ShapeRenderer sr;
+	private PuzzleGameSystem puzzleGame; 
 
-
-	// Static Textures
-	static final Texture triTexReg = new Texture(Gdx.files.internal("images/triangle-symbol-reg.png"));
-	static final Texture triTexInv = new Texture(Gdx.files.internal("images/triangle-symbol-inv.png"));
-	static final Texture cirTex = new Texture(Gdx.files.internal("images/circle-symbol.png"));
-	static final Texture cirTexHollow = new Texture(Gdx.files.internal("images/circle-symbol-02.png"));
-	
 
 	public MainScreen(GenePuzzler genePuzzler){
 		parent = genePuzzler; 
 		stage = new Stage(new ScreenViewport());
-		
-		cellBoxesGroup = new Group();
-		
-		// Skinning with AssetManager
-		parent.assMan.queueAddSkin();
-		parent.assMan.manager.finishLoading();
-		skin = parent.assMan.manager.get("skin/neon-ui-changed.json");
-		
-		sr = new ShapeRenderer();
+		puzzleGame = new PuzzleGameSystem(parent);
+
+		stage.addActor(puzzleGame);
+
 
 	}
 
 	@Override
 	public void show() {
 		Gdx.input.setInputProcessor(stage);	// This is a trial, may move
-		Table superTable = new Table();
-		Table gridTable = new Table();
-		Table outputTable = new Table();
-		superTable.setFillParent(true);
-		
-		superTable.padTop(stage.getHeight()*0.1f);
-		superTable.padLeft(stage.getWidth()*0.1f); 
-		superTable.left().top();
-		outputTable.center().top().padLeft(stage.getWidth()*0.05f);
-		
-		stage.addActor(superTable);
-		superTable.add(gridTable);
-		superTable.add(outputTable);
-		
-		superTable.setFillParent(true);
-		gridTable.setDebug(true);
-		outputTable.setDebug(false);
-			
-		
-		// Simple Widget
-		final CheckBox cellBox0 = new CheckBox(null, skin);
-		final CheckBox cellBox1 = new CheckBox(null, skin);
-		final CheckBox cellBox2 = new CheckBox(null, skin);
-		final CheckBox cellBox3 = new CheckBox(null, skin);
-		final CheckBox cellBox4 = new CheckBox(null, skin);
-		final CheckBox cellBox5 = new CheckBox(null, skin);
-		
-		
-		final Label cellOutLabel0 = new Label(setCellOut(cellBox0),skin);
-		final Label cellOutLabel1 = new Label(setCellOut(cellBox1),skin);
-		final Label cellOutLabel2 = new Label(setCellOut(cellBox2),skin);
-		final Label cellOutLabel3 = new Label(setCellOut(cellBox3),skin);
-		final Label cellOutLabel4 = new Label(setCellOut(cellBox4),skin);
-		final Label cellOutLabel5 = new Label(setCellOut(cellBox5),skin);
-		
-		// Creating the triangles box
-		final Label decimalOutLable = new Label(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()),skin);
-		
-		
-		///final Image triImage0 = new Image();
-		final Image image0 = new Image();
-		final Image image1 = new Image();
-		final Image image2 = new Image();
-		
-		
-		/*
-		triImage0.setSize(triTex.getWidth(), triTex.getHeight());
-		triImage1.setSize(triTex.getWidth(), triTex.getHeight());
-		
-		cirImage0.setSize(cirTex.getWidth(), cirTex.getHeight());
-		*/
-		
-		image0.setVisible(false);
-		image1.setVisible(false);
-		image2.setVisible(false);
-		
-		// Listeners for Grid
-		cellBox0.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {
-				
-				image0.setVisible(true);
-				int i = getInteger(cellBox0,cellBox1);
-				setNucliotideSymbol(i,image0);
-				//cellOutLabel0.setText(setCellOut(cellBox0));
-				//decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		cellBox1.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {				
-				int i = getInteger(cellBox0,cellBox1);
-				setNucliotideSymbol(i,image0);
-				//cellOutLabel1.setText(setCellOut(cellBox1));
-				//decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		cellBox2.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {				
-				cellOutLabel2.setText(setCellOut(cellBox2));
-				decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		cellBox3.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {				
-				cellOutLabel3.setText(setCellOut(cellBox3));
-				decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		cellBox4.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {				
-				cellOutLabel4.setText(setCellOut(cellBox4));
-				decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		cellBox5.addListener( new EventListener() {
-			@Override
-			public boolean handle(Event event) {				
-				cellOutLabel5.setText(setCellOut(cellBox5));
-				decimalOutLable.setText(setDecimalOut(cellOutLabel0.getText(),cellOutLabel1.getText(),cellOutLabel2.getText(),cellOutLabel3.getText(),cellOutLabel4.getText(),cellOutLabel5.getText()));
-				return false;
-			}
-		});
-		
-		
-		
-		
-		
-		// Grid Table		
-		gridTable.add(cellBox0);
-		gridTable.add(cellBox1);
-		gridTable.row();
-		gridTable.add(cellBox2);
-		gridTable.add(cellBox3);
-		gridTable.row();
-		gridTable.add(cellBox4);
-		gridTable.add(cellBox5);
-		gridTable.row();
-		gridTable.add(new Label("",skin));
-		
-		
-		// Output Table
-		//outputTable.add(cellOutLabel0).width(100).height(100);
-		outputTable.add(image0).width(100).height(100);	
-		//outputTable.add(triImage);
-		outputTable.row();
-		outputTable.add(image1).width(100).height(100);
-		//outputTable.add(cellOutLabel3).width(100).height(100);
-		outputTable.row();
-		outputTable.add(image2).width(100).height(100);
-		//outputTable.add(cellOutLabel5).width(100).height(100);
-		outputTable.row();
-		outputTable.add(decimalOutLable);
-		
 	}
 
 	@Override
 	public void render(float delta) {
-
-
-
-
 		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		
-		
 		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
-
-		
-		//engine.update(delta);
 
 	}
 
@@ -243,56 +66,6 @@ public class MainScreen implements Screen {
 	@Override
 	public void dispose() {
 		stage.dispose();
-	}
-
-	
-	
-	public String setCellOut(CheckBox c){
-		if(c.isChecked()){
-			return "1";
-		} else {
-			return "0";
-		}
-	}
-	
-	public void setNucliotideSymbol(int i, Image image){
-		image.setColor(Color.GOLD);
-		
-		switch(i){
-		case 0:
-			// set for U
-			image.setDrawable(new TextureRegionDrawable(new TextureRegion(triTexInv)));
-			break;
-		case 1:
-			// set for C
-			image.setDrawable(new TextureRegionDrawable(new TextureRegion(cirTex)));
-			break;
-		case 2:
-			// set for A
-			image.setDrawable(new TextureRegionDrawable(new TextureRegion(triTexReg)));
-			break;
-		case 3:
-			// set for G
-			image.setDrawable(new TextureRegionDrawable(new TextureRegion(cirTexHollow)));
-			break;
-		}
-	}
-	
-	public int getInteger(CheckBox c0, CheckBox c1){
-		int n0 = c0.isChecked() ? 1 : 0;
-		int n1 = c1.isChecked() ? 1 : 0;
-		int n = Integer.parseInt(""+n0+n1,2);
-		
-		return n;
-	}
-	
-	public String setDecimalOut(com.badlogic.gdx.utils.StringBuilder stringBuilder, com.badlogic.gdx.utils.StringBuilder stringBuilder2, com.badlogic.gdx.utils.StringBuilder stringBuilder3, com.badlogic.gdx.utils.StringBuilder stringBuilder4, com.badlogic.gdx.utils.StringBuilder stringBuilder5, com.badlogic.gdx.utils.StringBuilder stringBuilder6){
-		String cb = ""+stringBuilder+stringBuilder2 + stringBuilder3 + stringBuilder4+ stringBuilder5+ stringBuilder6;
-		int ci = Integer.parseInt(cb, 2);
-		char[] aminoCodonCodes = {
-				'f','f','l','l','s','s','s','s','y','y','*','*','c','c','*','w','l','l','l','l','p','p','p','p','h','h','q','q','r','r','r','r','i','i','i','m','t','t','t','t','n','n','k','k','s','s','r','r','v','v','v','v','a','a','a','a','d','d','e','e','g','g','g','g'
-		};
-		return ""+aminoCodonCodes[ci];
 	}
 
 }
--- src/com/genepuzzler/game/systems/PuzzleGameSystem.java
+++ src/com/genepuzzler/game/systems/PuzzleGameSystem.java
@@ -1,7 +1,297 @@
 package com.genepuzzler.game.systems;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Array;
+import com.genepuzzler.game.GenePuzzler;
 
-public class PuzzleGameSystem extends Actor{
-	
+public class PuzzleGameSystem extends Table{
+	GenePuzzler parent;
+
+	private Skin skin;
+
+
+	// Static Textures
+	static final Texture triTexReg = new Texture(Gdx.files.internal("images/triangle-symbol-reg.png"));
+	static final Texture triTexInv = new Texture(Gdx.files.internal("images/triangle-symbol-inv.png"));
+	static final Texture cirTex = new Texture(Gdx.files.internal("images/circle-symbol.png"));
+	static final Texture cirTexHollow = new Texture(Gdx.files.internal("images/circle-symbol-02.png"));
+
+
+	Table gridTable = new Table();
+	Table outputTable = new Table();
+	Table superTable = new Table();
+
+
+
+	// Simple Widget
+	final CheckBox cellBox0;
+	final CheckBox cellBox1 ;
+	final CheckBox cellBox2 ;
+	final CheckBox cellBox3 ;
+	final CheckBox cellBox4 ;
+	final CheckBox cellBox5 ;
+
+
+	Label decimalOutLable;
+
+
+	final Image image0 = new Image();
+	final Image image1 = new Image();
+	final Image image2 = new Image();
+
+	private int[] cellOutInt = {0,0,0,0,0,0};	
+			
+
+	public PuzzleGameSystem(GenePuzzler genePuzzler){
+		// Skinning with AssetManager
+		parent = genePuzzler;
+
+		parent.assMan.queueAddSkin();
+		parent.assMan.manager.finishLoading();
+		skin = parent.assMan.manager.get("skin/neon-ui-changed.json");
+
+
+
+		// Table Manager
+		setFillParent(true);
+
+		superTable.setFillParent(true);
+		superTable.padTop(80);
+		superTable.padLeft(80); 
+		superTable.left().top();
+
+
+		outputTable.center().top().padLeft(40);
+
+
+		superTable.add(gridTable);
+		superTable.add(outputTable);
+
+		addActor(superTable);
+
+
+		superTable.setFillParent(true);
+		gridTable.setDebug(false);
+		outputTable.setDebug(false);
+
+
+
+		// Widget Managing
+
+		image0.setVisible(false);
+		image1.setVisible(false);
+		image2.setVisible(false);
+
+
+		cellBox0 = new CheckBox(null, skin);
+		cellBox1 = new CheckBox(null, skin);
+		cellBox2 = new CheckBox(null, skin);
+		cellBox3 = new CheckBox(null, skin);
+		cellBox4 = new CheckBox(null, skin);
+		cellBox5 = new CheckBox(null, skin);
+
+
+		// Creating the Amino Output Label
+//		cellOutLabel0 = new Label(setCellOut(cellBox0),skin);
+//		cellOutLabel1 = new Label(setCellOut(cellBox1),skin);
+//		cellOutLabel2 = new Label(setCellOut(cellBox2),skin);
+//		cellOutLabel3 = new Label(setCellOut(cellBox3),skin);
+//		cellOutLabel4 = new Label(setCellOut(cellBox4),skin);
+//		cellOutLabel5 = new Label(setCellOut(cellBox5),skin);
+
+		
+		
+		
+
+		// Adding to the tables
+		// Grid Table		
+		gridTable.add(cellBox0);
+		gridTable.add(cellBox1);
+		gridTable.row();
+		gridTable.add(cellBox2);
+		gridTable.add(cellBox3);
+		gridTable.row();
+		gridTable.add(cellBox4);
+		gridTable.add(cellBox5);
+		gridTable.row();
+		gridTable.add(new Label("",skin));
+
+		decimalOutLable	= new Label("",skin);
+
+		// Output Table
+		outputTable.add(image0).width(100).height(100);	
+		outputTable.row();
+		outputTable.add(image1).width(100).height(100);
+		outputTable.row();
+		outputTable.add(image2).width(100).height(100);
+		outputTable.row();
+		outputTable.add(decimalOutLable);
+
+	}
+
+
+	@Override
+	public void act(float delta){
+		super.act(delta);
+
+		// Listeners for Grid
+		cellBox0.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {
+				// Image managing
+				image0.setVisible(true);
+				int i = getInteger(cellBox0,cellBox1);
+				setNucliotideSymbol(i,image0);
+				
+				// Codon Updating
+				cellOutInt[0] = setCellOut(cellBox0);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+		cellBox1.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {				
+				// Image managing
+				image0.setVisible(true);
+				int i = getInteger(cellBox0,cellBox1);
+				setNucliotideSymbol(i,image0);
+				
+				// Codon Updating
+				cellOutInt[1] = setCellOut(cellBox1);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+		cellBox2.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {
+				// Image managing
+				image1.setVisible(true);
+				int i = getInteger(cellBox2,cellBox3);
+				setNucliotideSymbol(i,image1);
+				
+				// Codon Updating
+				cellOutInt[2] = setCellOut(cellBox2);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+		cellBox3.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {				
+				// Image managing
+				image1.setVisible(true);
+				int i = getInteger(cellBox2,cellBox3);
+				setNucliotideSymbol(i,image1);
+				
+				// Codon Updating
+				cellOutInt[3] = setCellOut(cellBox3);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+		cellBox4.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {			
+				// Image managing
+				image2.setVisible(true);
+				int i = getInteger(cellBox4,cellBox5);
+				setNucliotideSymbol(i,image2);
+				
+				// Codon Updating
+				cellOutInt[4] = setCellOut(cellBox4);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+		cellBox5.addListener( new EventListener() {
+			@Override
+			public boolean handle(Event event) {	
+				// Image managing
+				image2.setVisible(true);
+				int i = getInteger(cellBox4,cellBox5);
+				setNucliotideSymbol(i,image2);
+				
+				// Codon Updating
+				cellOutInt[5] = setCellOut(cellBox5);
+				decimalOutLable.setText(setDecimalOut(cellOutInt));
+				return false;
+			}
+		});
+
+
+
+
+
+
+
+	}
+
+
+	public int getInteger(CheckBox c0, CheckBox c1){
+		int n0 = c0.isChecked() ? 1 : 0;
+		int n1 = c1.isChecked() ? 1 : 0;
+		int n = Integer.parseInt(""+n0+n1,2);
+
+		return n;
+	}
+
+	public String setDecimalOut(int[] a){
+		StringBuilder binValue = new StringBuilder();
+		for(int i=0;i<a.length;i++){
+			binValue.append(a[i]);
+		}
+		System.out.println(binValue.toString());
+		int ci = Integer.parseInt(binValue.toString(), 2);
+		char[] aminoCodonCodes = {
+				'f','f','l','l','s','s','s','s','y','y','*','*','c','c','*','w','l','l','l','l','p','p','p','p','h','h','q','q','r','r','r','r','i','i','i','m','t','t','t','t','n','n','k','k','s','s','r','r','v','v','v','v','a','a','a','a','d','d','e','e','g','g','g','g'
+		};
+		return ""+aminoCodonCodes[ci];
+	}
+
+
+	public Integer setCellOut(CheckBox c){
+		if(c.isChecked()){
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	public void setNucliotideSymbol(int i, Image image){
+		image.setColor(Color.GOLD);
+
+		switch(i){
+		case 0:
+			// set for U
+			image.setDrawable(new TextureRegionDrawable(new TextureRegion(triTexInv)));
+			break;
+		case 1:
+			// set for C
+			image.setDrawable(new TextureRegionDrawable(new TextureRegion(cirTex)));
+			break;
+		case 2:
+			// set for A
+			image.setDrawable(new TextureRegionDrawable(new TextureRegion(triTexReg)));
+			break;
+		case 3:
+			// set for G
+			image.setDrawable(new TextureRegionDrawable(new TextureRegion(cirTexHollow)));
+			break;
+		}
+	}
+
+
 }
